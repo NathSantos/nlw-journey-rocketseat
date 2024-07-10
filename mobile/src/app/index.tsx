c03c90cddf16657e445215d54fe46f9c08071d7d
Nathália Santos <nathsts@motorola.com>
2024-07-10 13:34:07 -0400

    componente Input

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { Input } from '@/components/input';
 
 export default function Index() {
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Text className='text-zinc-400'>
-        Convide seus amigos e planeje sua próxima viagem!
+    <View className='flex-1 justify-center items-center px-5'>
+      <Image
+        source={require('@/assets/logo.png')}
+        className='h-8'
+        resizeMode='contain'
+      />
+      <Text className='text-zinc-400 font-regular text-center text-lg mt-3'>
+        Convide seus amigos e planeje sua{'\n'}próxima viagem!
       </Text>
+
+      <View className='w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800'>
+        <Input>
+          <Input.Field placeholder='Para onde?' />
+        </Input>
+      </View>
     </View>
   );
 }
