33a78a25f51909c47a85358091d5378c25113e6b
Nathália Santos <nathsts@motorola.com>
2024-07-10 14:24:32 -0400

    pagina inicial completa (com animação - aparecimento condicional dos campos)

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,4 +1,5 @@
 import { View, Text, Image } from 'react-native';
+import { useState } from 'react';
 import {
   MapPin,
   Calendar as IconCalendar,
@@ -11,7 +12,20 @@ import { colors } from '@/styles/colors';
 import { Input } from '@/components/input';
 import { Button } from '@/components/button';
 
+enum StepFormEnum {
+  TRIP_DETAILS = 1,
+  ADD_EMAIL = 2,
+}
+
 export default function Index() {
+  const [stepForm, setStepForm] = useState(StepFormEnum.TRIP_DETAILS);
+
+  function handleNextStepForm() {
+    if (stepForm === StepFormEnum.TRIP_DETAILS) {
+      return setStepForm(StepFormEnum.ADD_EMAIL);
+    }
+  }
+
   return (
     <View className='flex-1 justify-center items-center px-5'>
       <Image
@@ -26,28 +40,45 @@ export default function Index() {
       <View className='w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800'>
         <Input>
           <MapPin color={colors.zinc[400]} size={20} />
-          <Input.Field placeholder='Para onde?' />
+          <Input.Field
+            placeholder='Para onde?'
+            editable={stepForm === StepFormEnum.TRIP_DETAILS}
+          />
         </Input>
 
         <Input>
           <IconCalendar color={colors.zinc[400]} size={20} />
-          <Input.Field placeholder='Quando?' />
+          <Input.Field
+            placeholder='Quando?'
+            editable={stepForm === StepFormEnum.TRIP_DETAILS}
+          />
         </Input>
 
-        <View className='border-b py-3 border-zinc-800'>
-          <Button variant='secondary'>
-            <Button.Title>Alterar local/data</Button.Title>
-            <Settings2 color={colors.zinc[200]} size={20} />
-          </Button>
-        </View>
+        {stepForm === StepFormEnum.ADD_EMAIL && (
+          <>
+            <View className='border-b py-3 border-zinc-800'>
+              <Button
+                variant='secondary'
+                onPress={() => setStepForm(StepFormEnum.TRIP_DETAILS)}
+              >
+                <Button.Title>Alterar local/data</Button.Title>
+                <Settings2 color={colors.zinc[200]} size={20} />
+              </Button>
+            </View>
 
-        <Input>
-          <UserRoundPlus color={colors.zinc[400]} size={20} />
-          <Input.Field placeholder='Quem estará na viagem?' />
-        </Input>
+            <Input>
+              <UserRoundPlus color={colors.zinc[400]} size={20} />
+              <Input.Field placeholder='Quem estará na viagem?' />
+            </Input>
+          </>
+        )}
 
-        <Button>
-          <Button.Title>Continuar</Button.Title>
+        <Button onPress={handleNextStepForm}>
+          <Button.Title>
+            {stepForm === StepFormEnum.TRIP_DETAILS
+              ? 'Continuar'
+              : 'Confirmar Viagem'}
+          </Button.Title>
           <ArrowRight color={colors.lime[950]} size={20} />
         </Button>
       </View>
